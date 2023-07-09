bcffe5b35e227f609ca5c1b787a9fb9c331882c7
wsafight <984292420@qq.com>
2023-07-09 22:13:39 +0800

    #FEAT: add test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,21 @@
+// import { TreeHelper } from '../src';
+
+// const treeHelper = new TreeHelper({
+//   items: [
+//     {
+//       id: 1,
+//       name: '王世安',
+//     },
+//     {
+//       id: 2,
+//       name: '王世安',
+//       parentId: 1,
+//     },
+//   ],
+// });
+
 describe('Default cases', () => {
   test('hello world test', () => {
-    const s = 'hello Modern.js';
-    expect(s).toBe('hello Modern.js');
+    // expect(treeHelper).toBe();
   });
 });
